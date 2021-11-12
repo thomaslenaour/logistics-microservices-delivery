33843365c42972de1817a21ad1de6d30eef2094a
ludovicGendre <ludovic.gendre@g-mail.fr>
2021-11-12 16:28:56 +0100

    feat: add sendNotification

--- src/shipping/utils/order.ts
+++ src/shipping/utils/order.ts
@@ -2,13 +2,14 @@ import { OrderEntity } from '../../database/entities/order.entity';
 import { UpdateOrderDto } from '../../dtos/order.dto';
 import axios from 'axios';
 
-const apiUrl = 'https://blobfish-tech.herokuapp.com/api';
+const orderApiUrl = 'https://blobfish-tech.herokuapp.com/api';
+const clientApiUrl = 'https://fhemery-logistics.herokuapp.com/api/#/';
 
 export class OrderAction {
   public async checkService(): Promise<boolean> {
     try {
       require('axios');
-      await axios.get(`${apiUrl}/ping`);
+      await axios.get(`${orderApiUrl}/ping`);
       return true;
     } catch {
       throw new Error('invalid service command');
@@ -19,9 +20,22 @@ export class OrderAction {
     await this.checkService();
 
     orders.map(async (order) => {
-      await axios.patch<UpdateOrderDto>(`${apiUrl}/order/${order.orderId}`, {
-        status: 'pending',
-      });
+      await axios.patch<UpdateOrderDto>(
+        `${orderApiUrl}/order/${order.orderId}`,
+        {
+          status: 'pending',
+        },
+      );
     });
+  }
+
+  public async sendNotification(orders: OrderEntity[]) {
+    await Promise.all(
+      orders.map(async (order) => {
+        await axios.post<UpdateOrderDto>(
+          `${clientApiUrl}/client-order/${order.orderId}`,
+        );
+      }),
+    );
   }
 }
